2547ce8a621c8e7e59cd67d46989498eb68d5308
matheusboliveir <theus.braz18@gmail.com>
2025-08-26 02:02:29 -0300

    fix: Ajustando model de Dose para não enviar referencias na resposta

--- VacinaBtgApi/Models/Dose.cs
+++ VacinaBtgApi/Models/Dose.cs
@@ -1,13 +1,19 @@
-﻿namespace VacinaBtgApi.Models
+﻿using System.Text.Json.Serialization;
+
+namespace VacinaBtgApi.Models
 {
     public class Dose
     {
         public int Id { get; set; }
         public TipoDoseEnum tipo { get; set; }
         public int numero { get; set; }
+        [JsonIgnore]
         public int PessoaId { get; set; }
+        [JsonIgnore]
         public int VacinaId { get; set; }
+        [JsonIgnore]
         public Pessoa Pessoa { get; set; }
+        [JsonIgnore]
         public Vacina Vacina { get; set; }
     }
 }
